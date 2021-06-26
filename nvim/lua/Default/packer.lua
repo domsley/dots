3f15a163e9bcf7a9a6725048f0cc4ce195daf845
domsley <matousek.dominik@protonmail.com>
2021-06-26 17:47:15 +0200

    Refactoring and New plugins

--- nvim/lua/Default/packer.lua
+++ nvim/lua/Default/packer.lua
@@ -3,154 +3,157 @@ local fn = vim.fn
 local packer = nil
 
 local function packer_verify()
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
-    cmd 'packadd packer.nvim'
-  end
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
+        cmd 'packadd packer.nvim'
+    end
 end
 
 local function packer_startup()
-  if packer == nil then
-    packer = require'packer'
-    packer.init()
-  end
-
-  local use = packer.use
-  packer.reset()
-
-  -- Packer
-  use 'wbthomason/packer.nvim'
-
-  -- Language Servers
-  use {
-    'lspcontainers/lspcontainers.nvim',
-    requires = {
-      'neovim/nvim-lspconfig',
-      'nvim-lua/lsp_extensions.nvim',
-    },
-    config = function ()
-      require'Default.plugins.lsp'.init()
+    if packer == nil then
+        packer = require'packer'
+        packer.init()
     end
-  }
 
-  -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = 'TSUpdate',
-    config = function ()
-      require'Default.plugins.treesitter'.init()
-    end,
-  }
+    local use = packer.use
+    packer.reset()
 
-  -- Completion
-  use {
-    'hrsh7th/nvim-compe',
-    requires = {
-      {
-        'erkrnt/compe-tabnine',
-        run = './install.sh'
-      },
-      'wellle/tmux-complete.vim',
-      'hrsh7th/vim-vsnip'
-    },
-    config = function ()
-      require'Default.plugins.compe'.init()
-      require'Default.plugins.compe_tabnine'.init()
-    end
-  }
+    -- Packer
+    use 'wbthomason/packer.nvim'
 
-  -- Telescope
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-lua/popup.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = 'rmagatti/session-lens',
-    config = function ()
-      require'Default.plugins.telescope'.init()
-    end
-  }
+    -- Language Servers
+    use {
+        'lspcontainers/lspcontainers.nvim',
+        requires = {
+            'neovim/nvim-lspconfig',
+            'nvim-lua/lsp_extensions.nvim',
+        },
+        config = function ()
+            require'Default.plugins.lsp'.init()
+        end
+    }
 
-  -- NERDTree
-  use {
-    'preservim/nerdtree',
-    config = function ()
-        require'Default.plugins.nerdtree'.init()
-    end
-  }
+    -- Treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = 'TSUpdate',
+        config = function ()
+            require'Default.plugins.treesitter'.init()
+        end,
+    }
 
-  -- Themes
-  use {
-    'folke/tokyonight.nvim',
-    config = function ()
-      require 'Default.plugins.tokyonight'.init()
-    end
-  }
+    -- Completion
+    use {
+        'hrsh7th/nvim-compe',
+        requires = {
+            {
+                'erkrnt/compe-tabnine',
+                run = './install.sh'
+            },
+            'wellle/tmux-complete.vim',
+            'hrsh7th/vim-vsnip'
+        },
+        config = function ()
+            require'Default.plugins.compe'.init()
+            require'Default.plugins.compe_tabnine'.init()
+        end
+    }
 
-  -- Git Support
-  use 'rhysd/git-messenger.vim'
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = function ()
-      require'Default.plugins.gitsigns'.init()
-    end
-  }
+    -- Telescope
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-lua/popup.nvim'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = 'rmagatti/session-lens',
+        config = function ()
+            require'Default.plugins.telescope'.init()
+        end
+    }
 
-  -- Sessions
-  use {
-    'rmagatti/auto-session',
-    config = function ()
-      require'Default.plugins.auto_session'.init()
-    end
-  }
+    -- NERDTree
+    use {
+        'preservim/nerdtree',
+        config = function ()
+            require'Default.plugins.nerdtree'.init()
+        end
+    }
+    use 'ryanoasis/vim-devicons'
 
-  -- Utilities
-  use {
-    'hoob3rt/lualine.nvim',
-    config = function ()
-      require 'Default.plugins.lualine'.init()
-    end
-  }
-  use 'preservim/nerdcommenter'
-  use 'romgrk/nvim-treesitter-context'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'ThePrimeagen/vim-be-good'
-  use {
-    'voldikss/vim-floaterm',
-    config = function ()
-      require 'Default.plugins.floaterm'.init()
-    end
-  }
-  use {
-    'takac/vim-hardtime', -- see http://vimcasts.org/blog/2013/02/habit-breaking-habit-making
-    config = function ()
-      require'Default.plugins.hardtime'.init()
-    end
-  }
+    -- Themes
+    use {
+        'folke/tokyonight.nvim',
+        config = function ()
+            require 'Default.plugins.tokyonight'.init()
+        end
+    }
 
-  -- VimWiki + Zettelkasten
-  use {
-    'michal-h21/vim-zettel',
-    requires = {
-      {
-        'junegunn/fzf',
-        run = function () vim.fn['fzf#install']() end
-      },
-      'junegunn/fzf.vim',
-      'vimwiki/vimwiki'
-    },
-    config = function ()
-      require'Default.plugins.zettel'.init()
-    end
-  }
+    -- Git Support
+    use 'rhysd/git-messenger.vim'
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function ()
+            require'Default.plugins.gitsigns'.init()
+        end
+    }
+
+    -- Sessions
+    use {
+        'rmagatti/auto-session',
+        config = function ()
+            require'Default.plugins.auto_session'.init()
+        end
+    }
+
+    -- Utilities
+    use {
+        'hoob3rt/lualine.nvim',
+        config = function ()
+            require 'Default.plugins.lualine'.init()
+        end
+    }
+    use 'preservim/nerdcommenter'
+    use 'romgrk/nvim-treesitter-context'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'Yggdroot/indentLine'
+    use 'ThePrimeagen/vim-be-good'
+    use 'leafOfTree/vim-vue-plugin'
+    use {
+        'voldikss/vim-floaterm',
+        config = function ()
+            require 'Default.plugins.floaterm'.init()
+        end
+    }
+    use {
+        'takac/vim-hardtime', -- see http://vimcasts.org/blog/2013/02/habit-breaking-habit-making
+        config = function ()
+            require'Default.plugins.hardtime'.init()
+        end
+    }
+
+    -- VimWiki + Zettelkasten
+    use {
+        'michal-h21/vim-zettel',
+        requires = {
+            {
+                'junegunn/fzf',
+                run = function () vim.fn['fzf#install']() end
+            },
+            'junegunn/fzf.vim',
+            'vimwiki/vimwiki'
+        },
+        config = function ()
+            require'Default.plugins.zettel'.init()
+        end
+    }
 end
 
 local function init()
-  packer_verify()
-  packer_startup()
+    packer_verify()
+    packer_startup()
 end
 
 return {
-  init = init
+    init = init
 }
