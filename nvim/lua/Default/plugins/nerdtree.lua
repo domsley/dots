fb921d11e33e0461f6a8120b69e8f53fef1dff01
domsley <matousek.dominik@protonmail.com>
2021-06-26 20:25:08 +0200

    Preview image and new settings for NerdTree

--- nvim/lua/Default/plugins/nerdtree.lua
+++ nvim/lua/Default/plugins/nerdtree.lua
@@ -2,7 +2,17 @@ local function init()
     local map = vim.api.nvim_set_keymap
     local options = { noremap = true }
 
+    -- TODO: Rewrite to Lua
+    -- Open nerd tree at the current file or close nerd tree if pressed again.
+    --nnoremap <silent> <expr> <Leader>n g:NERDTree.IsOpen() ? "\:NERDTreeClose<CR>" : bufexists(expand('%')) ? "\:NERDTreeFind<CR>" : "\:NERDTree<CR>"
+
     map('n', '<F4>', '<CMD>NERDTreeToggle<CR>', options)
+
+    vim.cmd([['
+    let g:NERDTreeShowHidden=1
+    let g:NERDTreeAutoDeleteBuffer=1
+    ']]);
+
 end
 
 return {
